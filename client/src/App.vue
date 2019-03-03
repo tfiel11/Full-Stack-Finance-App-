5960f585684de671db75a590d445656f826c3535
tfiel11 <ty.fielding@gmail.com>
2019-03-03 12:56:13 -0700

    may have botched the whole project, switched everything to the new vue-cli

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Lets do this"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
